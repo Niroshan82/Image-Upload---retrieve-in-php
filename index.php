035965fab035c3fab0864dd2af81282c2402fda2
Niroshan <knniroshan82@outlook.com>
2018-08-28 01:19:52 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
+	<div class="container">
+		<div class="upfrm">
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select Image File to Upload:
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+</form>
+	</div>
+
+<div class="gallery">
+	<h2>Upload images</h2>
+	<?php
+// Include the database configuration file
+include 'dbConfig.php';
+
+// Get images from the database
+$query = $db->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+
+if($query->num_rows > 0){
+    while($row = $query->fetch_assoc()){
+        $imageURL = 'uploads/'.$row["file_name"];
+?>
+    <img src="<?php echo $imageURL; ?>" alt="" />
+<?php }
+}else{ ?>
+    <p>No image(s) found...</p>
+<?php } ?>
+	
+	</div>
+	</div>
+</body>
+</html>
